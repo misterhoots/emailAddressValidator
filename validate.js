9daf6a453c876b3273a6929df63c7dee482e2a01
Mark Saunders <mark@misterhoots.com>
2017-03-17 01:27:54 +0000

    edited comments

--- validate.js
+++ validate.js
@@ -1,5 +1,5 @@
 /*
-*   Title: My Simple email vallidator
+*   Title: My Simple email validator
 *   Author: Mark Saunders
 *   email: mark@misterhoots.com
 */
@@ -11,9 +11,9 @@ function getEmailAddress () {
 function validateEmailAddress (myEmailAddress) {
    var at = myEmailAddress.indexOf('@');
    var dot = myEmailAddress.indexOf('.');
-   // We need to get number of '@' symbol to check if equal to 1 in our condition. Also need to set to 0 value in myEmailAddress.
+   // We need to get number of '@' symbol to check if equal to 1 in our condition. Need to set to 0 if no value in myEmailAddress.
    var atCount = myEmailAddress.match(/@/g) === null || undefined? 0 : myEmailAddress.match(/@/g).length;
-    // We need to get number of '.' symbol to check if equal to 1 in our condition.  Also need to set to 0 value in myEmailAddress.
+    // We need to get number of '.' symbol to check if equal to 1 in our condition. Need to set to 0 if no value in myEmailAddress.
    var dotCount = myEmailAddress.match(/\./g) === null || undefined? 0 : myEmailAddress.match(/\./g).length;
 
    if ((at > 0) && (dot > (at + 1)) && (dot !== (myEmailAddress.length - 1)) && (atCount === 1) && (dotCount === 1)) {
